322f623675e41272142be3c30b71cd60f86f9bbb
nico.ortiz.a <nico.ortiz.a@gmail.com>
2019-10-04 11:54:45 -0500

    fluter prueba seminario

---
+++ Test Suites/Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>nicolas.ortiz.work@gmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4695ad55-eff2-4f3d-8e9c-01d570ee8122</testSuiteGuid>
+   <testCaseLink>
+      <guid>58483f00-f0ce-472d-a8ad-2d127b3eef2e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case fluutter</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
